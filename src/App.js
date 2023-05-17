738c0846aa7ef616e77dfafbc50ec35ef19e0036
Martin Rudzis <m19rudzis@gmail.com>
2023-05-17 16:38:58 -0700

    course info step9

--- src/App.js
+++ src/App.js
@@ -29,31 +29,53 @@ const Course = ({ course }) =>
   </div>
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'Redux',
-        exercises: 11
-      }
-    ]
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        },
+        {
+          name: 'Redux',
+          exercises: 11
+        },
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-    <Course course={course} />
+    <div>
+      {courses.map(course =>
+        <Course key={course.id} course={course} />
+      )}
+    </div>
   )
 }
 
