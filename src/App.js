bf525d0dbf59feda64635ad9a1f089663111b1b3
Martin Rudzis <m19rudzis@gmail.com>
2023-05-17 16:50:16 -0700

    separate module

--- src/App.js
+++ src/App.js
@@ -1,32 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ parts }) => 
-  <b>
-    Total exercises = {
-      parts.map(parts => parts.exercises).reduce(
-        (sum, exerciseCount) => sum + exerciseCount, 0
-      )
-    }
-  </b>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part =>
-      <Part key={part.name} part={part} />
-    )}     
-  </>
-
-const Course = ({ course }) => 
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
