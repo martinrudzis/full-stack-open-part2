f89484e8e7c2a13139f92f48f40628268e73859b
Martin Rudzis <m19rudzis@gmail.com>
2023-05-17 12:01:21 -0700

    course info step6

--- src/App.js
+++ src/App.js
@@ -9,40 +9,43 @@ const Part = ({ part }) =>
 
 const Content = ({ parts }) => 
   <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
+    {parts.map(part =>
+      <Part key={part.name} part={part} />
+    )}     
   </>
 
+const Course = ({ course }) => 
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+  </div>
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      },
+      {
+        name: 'Redux',
+        exercises: 11
+      }
+    ]
+  }
 
   return (
-    <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
-    </div>
+    <Course course={course} />
   )
 }
 
